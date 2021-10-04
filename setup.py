a651214d9cf4a568e74df260ba3eb35c96b8f6a2
kishor <kishorbrindha18@gmail.com>
2021-10-04 21:48:12 +0530

    all commit

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="kishor",
+    description="A small package for  ml pipeline demo",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kishorsumathi/dvc_practice",
+    author_email="kishorbrindha18@gmail.com",
+    packages=["src"],
+    python_requires=">=3.7",
+    install_requires=[
+        'dvc',
+        'pandas',
+        'scikit-learn'
+    ]
+)
