915ba9f23c74c146e7bf9a0630eb757491b37783
kishor.G <65946231+kishorsumathi@users.noreply.github.com>
2021-10-13 22:07:08 +0530

    CHANGES IN SETUP.PY

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setup(
     name="src",
-    version="0.0.1",
+    version="0.0.2",
     author="kishor",
     description="A small package for  ml pipeline demo",
     long_description=long_description,
